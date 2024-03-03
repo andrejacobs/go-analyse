be4ca4aa9db592a827f2e0cd3332f51c1c8f52de
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-03 18:50:54 +0000

    Can load languages from a csv file

--- internal/alphabet/load.go
+++ internal/alphabet/load.go
@@ -1,7 +1,63 @@
 package alphabet
 
-//AJ### TODO: think about file format
-// // Load a language from an io.Reader
-// func Load(r io.Reader) (Language, error) {
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
-// }
+// Load a set of languages from an io.Reader.
+//
+// Expected CSV format in UTF-8: code,name,letters
+// Lines starting with a # is ignored.
+func LoadLanguages(r io.Reader) (LanguageMap, error) {
+	result := make(LanguageMap)
+	csvR := csv.NewReader(r)
+
+	for {
+		record, err := csvR.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse csv. %w", err)
+		}
+
+		if len(record) < 3 {
+			continue
+		}
+
+		if strings.HasPrefix(record[0], "#") {
+			continue
+		}
+
+		code := LanguageCode(record[0])
+		l := Language{
+			Name:    record[1],
+			Code:    code,
+			Letters: strings.ToLower(record[2]),
+		}
+
+		result[code] = l
+	}
+
+	return result, nil
+}
+
+// Load a set of languages from a UTF-8 encoded text file.
+// See LoadLanguages for more details.
+func LoadLanguagesFromFile(path string) (LanguageMap, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open %s. %w", path, err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to close %s. %v", path, err)
+		}
+	}()
+
+	return LoadLanguages(f)
+}
---
+++ internal/alphabet/load_test.go
@@ -0,0 +1,41 @@
+package alphabet_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/andrejacobs/go-analysis/internal/alphabet"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadLanguages(t *testing.T) {
+	r := strings.NewReader(`#code,name,letters
+en,English,abcdefghijklmnopqrstuvwxyz
+
+weird,Weird,éÅß
+coding,Coding,{}[]()/$
+
+# Override previous entry,,
+coding,Coding,{}[]/$^%
+`)
+
+	languages, err := alphabet.LoadLanguages(r)
+	require.NoError(t, err)
+	assert.Equal(t, 3, len(languages))
+
+	assert.Equal(t, languages["en"], alphabet.Language{Name: "English", Code: "en", Letters: "abcdefghijklmnopqrstuvwxyz"})
+	assert.Equal(t, languages["weird"], alphabet.Language{Name: "Weird", Code: "weird", Letters: "éåß"})
+	assert.Equal(t, languages["coding"], alphabet.Language{Name: "Coding", Code: "coding", Letters: `{}[]/$^%`})
+}
+
+func TestLoadLanguagesFromFile(t *testing.T) {
+	languages, err := alphabet.LoadLanguagesFromFile("testdata/languages.csv")
+	require.NoError(t, err)
+
+	assert.Contains(t, languages, alphabet.LanguageCode("en"))
+	assert.Contains(t, languages, alphabet.LanguageCode("af"))
+
+	// generated languages.go should be the exact same as loading "testdata/languages.csv"
+	assert.Equal(t, alphabet.Languages(), languages)
+}
