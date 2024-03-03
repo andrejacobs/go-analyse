d904f417a3b909cb4e3264a2a9d9b93db8647c39
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-03 18:02:19 +0000

    fixing linting issues

--- internal/alphabet/alphabet.go
+++ internal/alphabet/alphabet.go
@@ -4,20 +4,20 @@ import "strings"
 
 //go:generate go run generate_languages.go
 
-// LanguageCode describes an ISO 639 set 1 language code
+// LanguageCode describes an ISO 639 set 1 language code.
 type LanguageCode string
 
 // Language describes the alphabet letters found in a language.
 type Language struct {
-	// Name of the language (e.g. Afrikaans)
+	// Name of the language (e.g. Afrikaans).
 	Name string
-	// ISO 639 set 1 language code (e.g. af) https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes
+	// ISO 639 set 1 language code (e.g. af) https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes.
 	Code LanguageCode
-	// Letters (in UTF-8 and lowercase) found in the language
+	// Letters (in UTF-8 and lowercase) found in the language.
 	Letters string
 }
 
-// LanguageMap is used to map from an language code to info about the language
+// LanguageMap is used to map from an language code to info about the language.
 type LanguageMap map[LanguageCode]Language
 
 // ContainsRune returns true if the language contains the rune.
