f25f930c9c6ca8e97471f9bdfc0de2ca37285a91
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-04 18:36:11 +0000

    wip. experimenting with a possible solution for reading n-grams from an io.Reader

---
+++ text/ngrams/doc.go
@@ -0,0 +1,3 @@
+// Package ngrams calculates monograms (1-gram), bigrams (2-grams), trigrams (3-grams), quadgrams (4-grams) and
+// quintgrams (5-grams) for either letters or words given a corpora.
+package ngrams
---
+++ text/ngrams/frequency_test.go
@@ -0,0 +1,107 @@
+package ngrams_test
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"slices"
+	"strings"
+	"testing"
+	"unicode"
+
+	"github.com/andrejacobs/go-analysis/internal/alphabet"
+	"golang.org/x/exp/maps"
+)
+
+func TestWIP(t *testing.T) {
+	input := "he SHE 本語本 the sheri zo."
+
+	input = strings.ToLower(input)
+	// mono(input, alphabet.Languages()["en"])
+	bi(input, alphabet.Languages()["en"])
+	tri(input, alphabet.Languages()["en"])
+	quad(input, alphabet.Languages()["en"])
+}
+
+func mono(input string, lang alphabet.Language) {
+	fmt.Printf("monograms: %q\n", input)
+	result := make(map[rune]uint64)
+
+	for _, r := range input {
+		if lang.ContainsRune(r) {
+			count, exists := result[r]
+			if !exists {
+				result[r] = 1
+			} else {
+				result[r] = count + 1
+			}
+		}
+	}
+
+	keys := maps.Keys(result)
+	slices.Sort(keys)
+
+	for _, k := range keys {
+		fmt.Printf("%c = %d\n", k, result[k])
+	}
+
+	fmt.Println()
+}
+
+func bi(input string, lang alphabet.Language) {
+	fmt.Printf("bigrams: %q\n", input)
+	ngram(input, lang, 2)
+	fmt.Println()
+}
+
+func tri(input string, lang alphabet.Language) {
+	fmt.Printf("trigrams: %q\n", input)
+	ngram(input, lang, 3)
+	fmt.Println()
+}
+
+func quad(input string, lang alphabet.Language) {
+	fmt.Printf("quadgrams: %q\n", input)
+	ngram(input, lang, 4)
+	fmt.Println()
+}
+
+func ngram(input string, lang alphabet.Language, size int) {
+	buf := make([]rune, size)
+	pos := 0
+	count := 0
+
+	rd := bufio.NewReader(strings.NewReader(input))
+
+	for {
+		r, _, err := rd.ReadRune()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("err: %v\n", err)
+			break
+		}
+
+		if unicode.IsSpace(r) {
+			pos = 0
+			count = 0
+			continue
+		}
+
+		if !lang.ContainsRune(r) {
+			continue
+		}
+
+		buf[pos+count] = r
+		count++
+
+		if count == size {
+			fmt.Printf("%s\n", string(buf[pos:]))
+
+			copy(buf[pos:], buf[pos+1:])
+			pos = 0
+			count = size - 1
+		}
+	}
+}
