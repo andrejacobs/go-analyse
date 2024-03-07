1a74fa7aa9eeccb9603bd2a6d35a471ec945e023
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-07 18:35:09 +0000

    small improvements to alphabet package just before it will be promoted to be public

--- internal/alphabet/alphabet.go
+++ internal/alphabet/alphabet.go
@@ -13,16 +13,16 @@ type Language struct {
 	Name string
 	// ISO 639 set 1 language code (e.g. af) https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes.
 	Code LanguageCode
-	// Letters (in UTF-8 and lowercase) found in the language.
+	// Letters (in UTF-8 and in lowercase) found in the language.
 	Letters string
 }
 
-// LanguageMap is used to map from an language code to info about the language.
+// LanguageMap is used to map from a language code to info about the language.
 type LanguageMap map[LanguageCode]Language
 
 // ContainsRune returns true if the language contains the rune.
 // The letters of the language is expected to only contain the lowercase runes
-// that make up the alphabet and thus the specified rune is assumed to be a lowercase rune.
+// that make up the alphabet and thus the specified rune is assumed to be a lowercase rune as well.
 func (l Language) ContainsRune(r rune) bool {
 	return strings.ContainsRune(l.Letters, r)
 }
--- internal/alphabet/doc.go
+++ internal/alphabet/doc.go
@@ -1,2 +1,5 @@
-// Internal package alphabet provides support for describing alphabets in different languages
+// Package alphabet provides support for describing alphabets used in different languages.
+// This package considers an alphabet to mean a valid set of unicode runes that can be used in tasks
+// like determining n-grams. A letter means a unicode rune and is generally not a number or a
+// symbol (e.g @!$ etc.). Language means a set of identifyable writing letters (e.g. EN = English = Latin Alphabet).
 package alphabet
