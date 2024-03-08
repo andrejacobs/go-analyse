546599025542deefe99872ca4cc077010fb0f1ca
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-08 06:52:45 +0000

    wip. Token parsing

---
+++ text/ngrams/tokens.go
@@ -0,0 +1,83 @@
+package ngrams
+
+import (
+	"bufio"
+	"context"
+	"io"
+	"unicode"
+
+	"github.com/andrejacobs/go-analysis/text/alphabet"
+)
+
+// RecvTokenFunc will be called when a new token has been parsed from the input stream.
+// The parser will pass any encountered error to this function and you should assume that the parsing process
+// will stop and that no more tokens will be produced.
+// If this function returns an error then it will indicate to the parser to stop the parsing process.
+type RecvTokenFunc func(token string, err error) error
+
+// ParseLetterTokens is used to parse ngrams for letter combinations of the given tokenSize and language from the io.Reader.
+func ParseLetterTokens(ctx context.Context, input io.Reader, language alphabet.Language,
+	tokenSize int, recv RecvTokenFunc) error {
+
+	buf := make([]rune, tokenSize)
+	pos := 0
+	count := 0
+
+	rd := bufio.NewReader(input)
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				// Inform consumer of error
+				_ = recv("", err)
+				return err
+			}
+			break loop
+		default:
+			r, _, err := rd.ReadRune()
+			if err != nil {
+				if err == io.EOF {
+					// Done reading
+					break loop
+				}
+				// Inform consumer of error
+				_ = recv("", err)
+				return err
+			}
+
+			// Ignore white space
+			if unicode.IsSpace(r) {
+				pos = 0
+				count = 0
+				continue
+			}
+
+			// Ignore any runes not part of the language
+			if !language.ContainsRune(r) {
+				continue
+			}
+
+			buf[pos+count] = r
+			count++
+
+			// Did we parse enough runes for a full token?
+			if count == tokenSize {
+				token := string(buf[pos:])
+
+				copy(buf[pos:], buf[pos+1:])
+				pos = 0
+				count = tokenSize - 1
+
+				// Inform the consumer of a new token
+				err := recv(token, nil)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
+
+	return nil
+}
---
+++ text/ngrams/tokens_test.go
@@ -0,0 +1,60 @@
+package ngrams_test
+
+import (
+	"bufio"
+	"context"
+	"os"
+	"testing"
+
+	"github.com/andrejacobs/go-analysis/text/alphabet"
+	"github.com/andrejacobs/go-analysis/text/ngrams"
+	"github.com/andrejacobs/go-collection/collection"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseLetterTokens(t *testing.T) {
+	testCases := []struct {
+		desc      string
+		filename  string
+		tokenSize int
+		language  alphabet.Language
+		expected  collection.Set[string]
+		//TODO: support context cancel, with check
+		// raise an error
+		// set of ngrams check
+	}{
+		{
+			desc:      "Bigram - Alice",
+			filename:  "testdata/en-alice-partial.txt",
+			tokenSize: 2,
+			language:  alphabet.Languages()["en"],
+			expected:  collection.NewSetFrom([]string{"ae"}),
+		},
+	}
+	for _, tC := range testCases {
+		t.Run(tC.desc, func(t *testing.T) {
+			f, err := os.Open(tC.filename)
+			require.NoError(t, err)
+			defer f.Close()
+
+			ctx := context.Background()
+			r := bufio.NewReader(f)
+
+			result := collection.NewSet[string]()
+
+			err = ngrams.ParseLetterTokens(ctx, r, tC.language, tC.tokenSize,
+				func(token string, err error) error {
+					assert.Len(t, token, tC.tokenSize)
+					result.Insert(token)
+					return nil
+				})
+			require.NoError(t, err)
+
+			assert.Equal(t, tC.expected, result)
+		})
+	}
+}
+
+//TODO: Add an expected file that we load the set from
+// bigrams-en-alice-partial.txt
