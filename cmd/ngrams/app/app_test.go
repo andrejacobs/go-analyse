a3efabd217a5f98a63b05f24400e59c91d5dbacd
Andre Jacobs <andrejacobs@users.noreply.github.com>
2024-03-14 18:18:12 +0000

    added happy path tests. Testing against the monograms, bigrams and trigrams from the ngrams test data set.

--- cmd/ngrams/app/app_test.go
+++ cmd/ngrams/app/app_test.go
@@ -6,10 +6,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/andrejacobs/go-analyse/cmd/ngrams/app"
+	"github.com/andrejacobs/go-analyse/text/ngrams"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -25,6 +27,9 @@ func TestApp(t *testing.T) {
 
 	validLanguages := validLanguagesFile(t)
 	defer os.Remove(validLanguages)
+
+	outPath := tempOutputPath(t)
+	defer os.Remove(outPath)
 
 	testCases := []struct {
 		desc     string
@@ -81,6 +86,61 @@ func TestApp(t *testing.T) {
 		//------------------------
 		// Happy paths
 
+		{desc: "monograms en-control", args: fmt.Sprintf("-s 1 -o %s %s", outPath, inputENControl), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputENControl1)
+		}},
+
+		{desc: "bigrams en-control", args: fmt.Sprintf("-s 2 -o %s %s", outPath, inputENControl), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputENControl2)
+		}},
+
+		{desc: "monograms af-control", args: fmt.Sprintf("-a af -s 1 -o %s %s", outPath, inputAFControl), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputAFControl1)
+		}},
+
+		{desc: "bigrams af-control", args: fmt.Sprintf("-a af -s 2 -o %s %s", outPath, inputAFControl), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputAFControl2)
+		}},
+
+		{desc: "bigrams en-alice-partial", args: fmt.Sprintf("-s 2 -o %s %s", outPath, inputENAlice), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputENAlice2)
+		}},
+
+		{desc: "trigrams en-alice-partial", args: fmt.Sprintf("-s 3 -o %s %s", outPath, inputENAlice), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputENAlice3)
+		}},
+
+		{desc: "bigrams fr-alice-partial", args: fmt.Sprintf("-a fr -s 2 -o %s %s", outPath, inputFRAlice), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputFRAlice2)
+		}},
+
+		{desc: "trigrams fr-alice-partial", args: fmt.Sprintf("-a fr -s 3 -o %s %s", outPath, inputFRAlice), testFunc: func(t *testing.T) {
+			_, stdErr, err := runMain()
+			assert.NoError(t, err)
+			assert.Empty(t, stdErr)
+			compareTwoFrequencyTableFiles(t, outPath, outputFRAlice3)
+		}},
 	}
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
@@ -123,3 +183,46 @@ coding,Coding,{}[]()/$
 
 	return f.Name()
 }
+
+func tempOutputPath(t *testing.T) string {
+	return filepath.Join(os.TempDir(), "ngrams-unit-testing-out.csv")
+}
+
+func compareTwoFrequencyTableFiles(t *testing.T, a string, b string) {
+	aft, err := ngrams.LoadFrequenciesFromFile(a)
+	require.NoError(t, err)
+
+	bft, err := ngrams.LoadFrequenciesFromFile(b)
+	require.NoError(t, err)
+
+	ae := aft.EntriesSortedByCount()
+	be := bft.EntriesSortedByCount()
+	assert.Equal(t, len(ae), len(be), "not the same number of rows")
+
+	for i, af := range ae {
+		bf := be[i]
+		assert.Equal(t, af.Token, bf.Token)
+		assert.Equal(t, af.Count, bf.Count)
+		assert.InEpsilon(t, af.Percentage, bf.Percentage, 0.000000001)
+	}
+}
+
+const (
+	ngramTestData = "../../../text/ngrams/testdata/"
+
+	inputENControl   = ngramTestData + "en-control.txt"
+	outputENControl1 = ngramTestData + "freq-1-en-control.csv"
+	outputENControl2 = ngramTestData + "freq-2-en-control.csv"
+
+	inputAFControl   = ngramTestData + "af-control.txt"
+	outputAFControl1 = ngramTestData + "freq-1-af-control.csv"
+	outputAFControl2 = ngramTestData + "freq-2-af-control.csv"
+
+	inputENAlice   = ngramTestData + "en-alice-partial.txt"
+	outputENAlice2 = ngramTestData + "freq-2-en-alice.csv"
+	outputENAlice3 = ngramTestData + "freq-3-en-alice.csv"
+
+	inputFRAlice   = ngramTestData + "fr-alice-partial.txt"
+	outputFRAlice2 = ngramTestData + "freq-2-fr-alice.csv"
+	outputFRAlice3 = ngramTestData + "freq-3-fr-alice.csv"
+)
